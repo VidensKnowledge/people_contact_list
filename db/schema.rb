91e912e5b456960b20a64012e2af38721e433dd7
ZaneN8 <zanelnewsom@gmail.com>
2020-08-25 17:00:08 -0600

    cleaned up display

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_204854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "people", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "age"
-    t.string "hair_color"
-    t.string "eye_color"
-    t.string "gender"
-    t.string "alive"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |i|
+15.times do |i|
   user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
   age: rand(18..80), hair_color: Faker::Color.color_name, eye_color: Faker::Color.color_name, 
   gender: Faker::Gender.binary_type, alive: 'Alive')
